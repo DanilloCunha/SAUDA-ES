ca94ffef15589d9515aaa0eccce395ab1d144f9a
Danillo Cunha <107063327+DanilloCunha@users.noreply.github.com>
2024-11-25 21:33:23 -0300

    MENSAGEM DE SAUDAÇÃO

---
+++ hello.rb
@@ -0,0 +1,5 @@
+# Este programa pergunta o nome do usuário e imprime uma saudação.
+
+puts "Qual é o seu nome?"
+nome = gets.chomp # Recebe o nome do usuário
+puts "Olá, #{nome}! Bem-vindo ao mundo do Ruby!"
